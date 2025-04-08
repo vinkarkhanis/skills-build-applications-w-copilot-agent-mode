109a7acc7fc26494eccb3ef4f2acb46c6e192fc6
Vinayak Karkhanis <karkhanisvinayak@outlook.com>
2025-04-08 12:06:25 +0000

    step 3

---
+++ octofit-tracker/backend/octofit_tracker/models.py
@@ -0,0 +1,23 @@
+from djongo import models
+
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+
+class Team(models.Model):
+    name = models.CharField(max_length=255)
+    members = models.JSONField()
+
+class Activity(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    type = models.CharField(max_length=255)
+    duration = models.IntegerField()
+
+class Leaderboard(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    score = models.IntegerField()
+
+class Workout(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
