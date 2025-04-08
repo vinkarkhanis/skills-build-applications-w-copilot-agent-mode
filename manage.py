01824dd4a04260b1351050fc66b93ba29983c5bc
Vinayak Karkhanis <karkhanisvinayak@outlook.com>
2025-04-08 12:10:02 +0000

    step4

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "octofit_tracker.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "octofit-tracker.backend.octofit_tracker.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
